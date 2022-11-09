ffd8b12fb5e16b7bb01a4ade1906bd107bf82224
techguy16 <88870951+techguy16@users.noreply.github.com>
2022-11-10 07:33:05 +1300

    Update lixit-spreadsheet.sh

--- aurpkgs/lixit-spreadsheet.sh
+++ aurpkgs/lixit-spreadsheet.sh
@@ -1,4 +1,9 @@
-sudo apt-get install build-essential qtcreator qt5-default git
-git clone https://github.com/lixit/spreadsheet.git
+echo "Installing Dependencies..."
+sudo apt-get install build-essential qtcreator qt5-default git > /dev/null
+
+echo "Cloning Lixit Spreadsheet repository..."
+git clone https://github.com/lixit/spreadsheet.git > /dev/null
+
+echo "Compiling Lixit Spreadsheet..."
 cd spreadsheet
 qmake spreadsheet.pro
