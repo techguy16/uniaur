5477f66abfacf2ff4d6af6e05b2eb15af077a423
techguy16 <88870951+techguy16@users.noreply.github.com>
2022-11-10 07:51:21 +1300

    Update install-uniaur.sh

--- install-uniaur.sh
+++ install-uniaur.sh
@@ -5,12 +5,15 @@ sudo apt install wget git > /dev/null
 
 echo "Downloading Uniaur Script..."
 wget -q https://raw.githubusercontent.com/techguy16/uniaur/main/uniaur > /dev/null
+sleep 2
 
 echo "Making Script Executable..."
 sudo chmod +x ./uniaur > /dev/null
+sleep 2
 
 echo "Copying Files..."
 sudo mv ./uniaur /usr/bin > /dev/null
+sleep 2
 
 echo "HOPEFULLY uniaur is installed"
 echo "Have a nice day :)"
