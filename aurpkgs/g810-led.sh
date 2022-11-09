c3711d77d9877093bdd0d19204ec9cd5e0a970ed
techguy16 <88870951+techguy16@users.noreply.github.com>
2022-11-10 07:27:39 +1300

    Update g810-led.sh

--- aurpkgs/g810-led.sh
+++ aurpkgs/g810-led.sh
@@ -1,5 +1,14 @@
-sudo apt-get install git g++ make libhidapi-dev libusb-1.0-0-dev
-git clone https://github.com/MatMoul/g810-led.git
+echo "Installing Dependencies..."
+sudo apt-get install git g++ make libhidapi-dev libusb-1.0-0-dev > /dev/null
+
+echo "Cloning g810-led repository..."
+git clone https://github.com/MatMoul/g810-led.git  > /dev/null
+
+echo "Compiling g810-led..."
 cd g810-led
-make bin
-sudo make install
+make bin > /dev/null
+
+echo "Installing g810-led..."
+sudo make install > /dev/null
+
+echo "Finished installing g810-led"
