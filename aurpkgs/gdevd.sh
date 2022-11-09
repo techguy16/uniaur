6c2348aca7414851528485d846015c0d6c28504f
techguy16 <88870951+techguy16@users.noreply.github.com>
2022-11-10 07:29:54 +1300

    Update gdevd.sh

--- aurpkgs/gdevd.sh
+++ aurpkgs/gdevd.sh
@@ -1,8 +1,18 @@
-git clone https://github.com/techguy16/gdevd
+echo "Cloning gdevd repository..."
+git clone https://github.com/techguy16/gdevd > /dev/null
+
+echo "Installing Rust..."
 cd gdevd
-sh ./rustup-init.sh --default-toolchain stable --profile default -q -y
-make
-sudo make install
-systemctl enable gdevd.service
-systemctl restart gdevd.service
-gdevctl --help
+sh ./rustup-init.sh --default-toolchain stable --profile default -q -y  > /dev/null
+
+echo "Compiling gdevd..."
+make > /dev/null
+
+echo "Installing gdevd..."
+sudo make install > /dev/null
+
+echo "Enabling Services..."
+systemctl enable gdevd.service > /dev/null
+systemctl restart gdevd.service > /dev/null
+
+echo "Finished installing gdevd"
