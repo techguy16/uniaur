910955ca06162872202dcd3a67edac9990e60562
techguy16 <88870951+techguy16@users.noreply.github.com>
2022-11-10 07:32:00 +1300

    Update linux.net.sh

--- aurpkgs/linux.net.sh
+++ aurpkgs/linux.net.sh
@@ -1,4 +1,11 @@
-git clone https://github.com/techguy16/Linux.NET
+echo "Cloning Linux.NET Repository..."
+git clone https://github.com/techguy16/Linux.NET > /dev/null
+
+echo "Compiling Linux.NET..."
 cd Linux.NET
-./configure ; make
+./configure ; make > /dev/null
+
+echo "Installing Linux.NET..."
 sudo make install
+
+echo "Finished installing Linux.NET"
