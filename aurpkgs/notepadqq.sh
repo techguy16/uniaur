13cfd542a3e441b14849962aa69f77a23cb4ea8d
techguy16 <88870951+techguy16@users.noreply.github.com>
2022-11-10 07:34:44 +1300

    Update notepadqq.sh

--- aurpkgs/notepadqq.sh
+++ aurpkgs/notepadqq.sh
@@ -1,6 +1,15 @@
-sudo apt-get install qt5-default qttools5-dev-tools qtwebengine5-dev libqt5websockets5-dev libqt5svg5 libqt5svg5-dev libuchardet-dev pkg-config git
-git clone --recursive https://github.com/notepadqq/notepadqq.git
+echo "Installing Dependencies..."
+sudo apt-get install qt5-default qttools5-dev-tools qtwebengine5-dev libqt5websockets5-dev libqt5svg5 libqt5svg5-dev libuchardet-dev pkg-config git > /dev/null
+
+echo "Cloning Notepadqq repository..."
+git clone --recursive https://github.com/notepadqq/notepadqq.git > /dev/null
+
+echo "Compiling Notepadqq..."
 cd notepadqq
-/configure --prefix /usr
-make
-sudo make install
+/configure --prefix /usr > /dev/null
+make > /dev/null
+
+echo "Installing Notepadqq..."
+sudo make install > /dev/null
+
+echo "Finished installing Notepadqq"
