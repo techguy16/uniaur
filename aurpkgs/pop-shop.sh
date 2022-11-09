b8da54eff5bddfbc0d907a9cf8c8b15d1447a521
techguy16 <88870951+techguy16@users.noreply.github.com>
2022-11-10 07:41:11 +1300

    Update pop-shop.sh

--- aurpkgs/pop-shop.sh
+++ aurpkgs/pop-shop.sh
@@ -1,3 +1,5 @@
+cd $HOME
+
 echo "Installing Dependencies..."
 sudo apt install python3 libgtk-3-dev libgranite-dev software-properties-common gettext libappstrea libpackagekit-glib2-devlibpolkit-gobject-1-devm-dev libflatpak-dev libgee-0.8-dev libhandy-1-dev libjson-glib-dev libpackagekit-glib2-dev libpolkit-gobject-1-dev libsoup2.4-dev libxml2-dev libxml2-utils meson valac > /dev/null
 
