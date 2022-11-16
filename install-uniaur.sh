5cd1576538cb1f143c9b531611fcbef3caa4a937
techguy16 <88870951+techguy16@users.noreply.github.com>
2022-11-16 16:27:48 +1300

    Update install-uniaur.sh

--- install-uniaur.sh
+++ install-uniaur.sh
@@ -1,24 +1,28 @@
 echo "Install uniaur"
 
-echo "Installing Dependencies..."
-sudo apt install wget git > /dev/null
+echo -ne 'Installing dependencies...           [                           (4%)]\r'
+sudo apt install wget git &> /dev/null
+sleep 2
 
-echo "Downloading Uniaur Scripts..."
+echo -ne 'Downloading uniaur scripts...        [#####                     (33%)]\r'
 wget -q https://raw.githubusercontent.com/techguy16/uniaur/main/uniaur > /dev/null
 wget -q https://raw.githubusercontent.com/techguy16/uniaur/main/update-uniaur > /dev/null
 
 sleep 2
-
-echo "Making Script Executable..."
+echo -ne 'Making script executable...          [#############             (66%)]\r'
 sudo chmod +x ./uniaur > /dev/null
-sudo chmod +x ./update-uniaur > /dev/null
-sleep 2
+sudo chmod +x ./update-uniaur > /dev/nul
 
-echo "Copying Files..."
+sleep 2
+echo -ne 'Copying new files...                 [#######################   (99%)]\r'
 sudo mv ./uniaur /usr/bin > /dev/null
 sudo mv ./update-uniaur /usr/bin > /dev/null
-sleep 2
 
-echo "HOPEFULLY uniaur is installed"
-echo "Have a nice day :)"
+sleep 2
+echo -ne 'Done!                                [#######################   (100%)]\r'
+echo -ne '\n'
+
+echo "uniaur is installed"
+echo "Please report issue reports (or request new features) @ https://github.com/techguy16/uniaur/issues"
+echo "Thank you for trying out uniaur!"
 
